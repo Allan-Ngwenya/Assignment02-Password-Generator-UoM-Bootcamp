d7fcf5f2d0d451c580ee945859dfe3766719632e
Mbongeni Ngwenya <mbongeniallanngwenya@hotmail.co.uk>
2021-03-07 21:37:27 +0000

    Adding comments to the script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Document method 'getElementById()' used to return Element object representing the element whose id property matches the specified strings below
+// Then called in the laast function if user selects the criteria
 const pwEl = document.getElementById("pw");
 const lenEl = document.getElementById("len");
 const upperEl = document.getElementById("upper");
@@ -6,10 +8,12 @@ const numberEl = document.getElementById("number");
 const symbolEl = document.getElementById("symbol");
 const generateEl = document.getElementById("generate");
 
-const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*()_+=";
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // String of uppercase characters to be included in password
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz"; // String of lowercase characters to be included in password
+const numbers = "0123456789"; // String of numeric characters to be included in password
+const symbols = "!@#$%^&*()_+="; // String of special characters to be included in password
+
+// The functions below from 'getLowercase()' to  'getSymbol()' are the criteria which the user may select and if selected the content is returned/added to the genrate password string.
 
 function getLowercase() {
     return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
@@ -19,13 +23,15 @@ function getUppercase() {
     return upperLetters[Math.floor(Math.random() * upperLetters.length)];
 }
 
-function getNumber() {
+function getNumber() { 
     return numbers[Math.floor(Math.random() * numbers.length)];
 }
 
 function getSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+// The function 'generatePassword()' will check if the criteria outlined above (and ticked in the form) is slected, then it will add it to the password.
 
 function generatePassword() {
     const len = lenEl.value;
@@ -53,8 +59,10 @@ function generatePassword() {
         password += x;
     }
 
-    pwEl.innerText = password;
+    pwEl.innerText = password; // innerTxt will set the content selected and I have assigned it to 'password'
 }
+
+// The function below is being used to push the criteria by using conditional statements to check if the options have been selcted above so the correct criteria can be displayed eventually
 
 function generateX() {
     const xs = [];
@@ -79,6 +87,8 @@ function generateX() {
     return xs[Math.floor(Math.random() * xs.length)];
 }
 
+//Add event listener to generate button uppon click action 
+
 generateEl.addEventListener("click", generatePassword);
 
 copyEl.addEventListener("click", () => {
@@ -89,5 +99,6 @@ copyEl.addEventListener("click", () => {
         return;
     }
 
+// Added event listener variable to generate button, testing to see if I can use the one above only
 var generateBtn = document.getElementById("generate");
 });
